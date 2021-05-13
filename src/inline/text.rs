ef1d3224c55fdf13c509181d7395e5c7dfad870f
meritozh <ah841814092@gmail.com>
2021-05-13 16:33:56 +0800

    fix(text): update text parser

--- src/inline/text.rs
+++ src/inline/text.rs
@@ -1,11 +1,22 @@
-use nom::IResult;
+use nom::{
+    character::complete::anychar,
+    combinator::{eof, map},
+    error::context,
+    multi::many0,
+    sequence::terminated,
+    IResult,
+};
 
 use crate::token::{Inline, Text};
 
-pub fn text(input: &str) -> Text {
-    Text { content: input }
+pub fn text(input: &str) -> IResult<&str, &str> {
+    context(
+        "text",
+        // TODO: anychar need convert line_ending to space.
+        terminated(map(many0(anychar), |v| &input[..v.len()]), eof),
+    )(input)
 }
 
-pub fn parse_text(input: &str) -> IResult<&str, Vec<Inline>> {
-    Ok(("", vec![Inline::Text(text(input))]))
+pub fn parse_text(input: &str) -> IResult<&str, Inline> {
+    map(text, |content| Inline::Text(Text { content }))(input)
 }
